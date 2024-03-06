b361d5932deab53326d74674ee5fe7e21fad2fb7
Henrique Reimao <henrique.reimao.hr@gmail.com>
2024-03-05 21:37:00 -0300

    - Create component Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HeaderComponent} from './components/header/header.component';
-import {PostComponent} from './components/post/post.component';
-import {ListaSugestivaComponent} from './components/lista-sugestiva/lista-sugestiva.component';
-import{MenuLateralComponent} from './components/menu-lateral/menu-lateral.component';
+import { HomeComponent } from './pages/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, PostComponent, ListaSugestivaComponent, MenuLateralComponent], 
+  imports: [RouterOutlet, HomeComponent,], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
